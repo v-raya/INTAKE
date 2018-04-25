4340548b6b681bf9e4b3301b68f5f52777593a74
Will Mitchell <will.mitchell@osi.ca.gov>
2018-04-25 13:22:24 -0700

    Climate cleanup in AgeInfo.jsx

--- app/javascript/common/AgeInfo.jsx
+++ app/javascript/common/AgeInfo.jsx
@@ -6,25 +6,23 @@ import sanitizeHtml from 'sanitize-html'
 const AgeInfo = ({dateOfBirth}) => {
   const sanitizedDob = sanitizeHtml(dateOfBirth, {allowedTags: []})
   const dob = moment.utc(sanitizedDob, 'YYYY-MM-DD')
-  const ageInYears = dob.isValid() && moment().diff(dob, 'years')
+
+  if (!dob.isValid()) { return false }
+
+  const ageInYears = moment().diff(dob, 'years')
 
   const genDobHtml = (dateOfBirth, sanitizedDob, dob) => {
+    if (dateOfBirth === `<em>${sanitizedDob}</em>`) {
+      return (<em>{dob.format('M/D/YYYY')}</em>)
+    }
     if (dateOfBirth !== sanitizedDob) {
-      if (dateOfBirth === `<em>${sanitizedDob}</em>`) {
-        return (
-          <em>{dob.format('M/D/YYYY')}</em>
-        )
-      } else {
-        return (
-          <span>{dob.format('M/D/')}<em>{dob.format('YYYY')}</em></span>
-        )
-      }
+      return (<span>{dob.format('M/D/')}<em>{dob.format('YYYY')}</em></span>)
     }
     return dob.format('M/D/YYYY')
   }
 
   return (
-    dob.isValid() && <div className='highlighted'>
+    <div className='highlighted'>
       <span>{`${ageInYears} yrs old (DOB:`}</span>
       {genDobHtml(dateOfBirth, sanitizedDob, dob)}
       <span>)</span>
