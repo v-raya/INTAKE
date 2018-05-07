b506266cb13c57d3d73551860819febf0c3cf5f8
Naga Pradeep Gajula <naga.gajula@osi.ca.gov>
2018-05-07 11:13:24 -0700

    HOT-1907 Clear the pending list within Create_Person_Complete rather than delete

--- spec/javascripts/reducers/pendingParticipantsReducerSpec.js
+++ spec/javascripts/reducers/pendingParticipantsReducerSpec.js
@@ -3,8 +3,6 @@ import {
   createPersonSuccess,
   createPersonFailure,
   createSnapshotPerson,
-  deletePersonSuccess,
-  deletePersonFailure,
 } from 'actions/personCardActions'
 import pendingParticipantsReducer from 'reducers/pendingParticipantsReducer'
 import {List, fromJS} from 'immutable'
@@ -28,25 +26,11 @@ describe('pendingParticipantsReducer', () => {
       const secondId = '3'
       const oldState = fromJS([firstId, secondId])
       const action = createPersonSuccess({id: secondId})
-      const expectedState = fromJS([firstId, secondId])
+      const expectedState = fromJS([firstId])
       expect(pendingParticipantsReducer(oldState, action)).toEqualImmutable(expectedState)
     })
     it('returns the last state on failure', () => {
       const action = createPersonFailure()
-      expect(pendingParticipantsReducer(List(), action)).toEqual(List())
-    })
-  })
-  describe('on DELETE_PERSON_COMPLETE', () => {
-    it('removes the existing person from the pending participants list', () => {
-      const firstId = '2'
-      const secondId = '3'
-      const oldState = fromJS([firstId, secondId])
-      const action = deletePersonSuccess(firstId)
-      const pendingParticipants = fromJS([secondId])
-      expect(pendingParticipantsReducer(oldState, action)).toEqualImmutable(pendingParticipants)
-    })
-    it('returns the last state on failure', () => {
-      const action = deletePersonFailure()
       expect(pendingParticipantsReducer(List(), action)).toEqual(List())
     })
   })
