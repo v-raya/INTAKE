826aa32514bfd2ed2c0b78e3a64084973cfa826a
manish pokharel <manish.pokharel@osi.ca.gov>
2018-05-10 10:30:35 -0700

    SNAP-34 Refactor the function isPlacementHome as it only takes one address

--- app/javascript/utils/isPlacementHome.js
+++ app/javascript/utils/isPlacementHome.js
@@ -1,5 +1,4 @@
 export const isPlacementHome = (address) => {
   if (!address) { return null }
-  return (([address].some((a) => a.getIn(['legacy_descriptor', 'legacy_table_name']) === 'PLC_HM_T')) ? 'Placement Home' : false)
+  return (address.getIn(['legacy_descriptor', 'legacy_table_name']) === 'PLC_HM_T') ? 'Placement Home' : false
 }
-
