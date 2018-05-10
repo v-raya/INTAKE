e364b89d971446a5bdb50e31f564be1f624a9d3c
Tyler Clemens <Tyler.Clemens@osi.ca.gov>
2018-05-10 15:38:52 -0700

    Update screening information specs to stub Ferb
    [#HOT-1952]

--- spec/features/screening/screening_information/character_limits_spec.rb
+++ spec/features/screening/screening_information/character_limits_spec.rb
@@ -5,15 +5,25 @@ require 'spec_helper'
 require 'factory_bot'
 
 feature 'screening information card' do
-  let(:screening) { FactoryBot.create(:screening) }
+  let(:screening) do
+    {
+      id: '1',
+      incident_address: {},
+      addresses: [],
+      cross_reports: [],
+      participants: [],
+      allegations: [],
+      safety_alerts: []
+    }
+  end
   let(:character_buffet) { 'C am-r\'o’n1234567890!@#$%^&*(),./;"[]' }
 
   scenario 'character limitations by field' do
-    stub_request(:get, intake_api_url(ExternalRoutes.intake_api_screening_path(screening.id)))
+    stub_request(:get, ferb_api_url(FerbRoutes.intake_screening_path(screening[:id])))
       .and_return(json_body(screening.to_json, status: 200))
     stub_empty_relationships
     stub_empty_history_for_screening(screening)
-    visit edit_screening_path(id: screening.id)
+    visit edit_screening_path(id: screening[:id])
 
     within '#screening-information-card.edit' do
       fill_in 'Title/Name of Screening', with: character_buffet
