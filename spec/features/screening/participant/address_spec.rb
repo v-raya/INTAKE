61a41c81183aa9fa2115945762de781b139a8119
Naga Pradeep Gajula <naga.gajula@osi.ca.gov>
2018-05-15 14:13:14 -0700

    HOT-1873 Fix feature spec

--- spec/features/screening/participant/address_spec.rb
+++ spec/features/screening/participant/address_spec.rb
@@ -25,45 +25,6 @@ feature 'Participant Address' do
     stub_empty_history_for_screening(screening)
   end
 
-  scenario 'addresses order' do
-    old_address =
-      FactoryBot.create(
-        :address,
-        id: '1',
-        street_address: '1 old st',
-        city: 'Old Springfield',
-        state: 'NY',
-        zip: '12345'
-      )
-    new_address =
-      FactoryBot.create(
-        :address,
-        id: '2',
-        street_address: '2 new st',
-        city: 'New Springfield',
-        state: 'NY',
-        zip: '12345'
-      )
-
-    marge = FactoryBot.create(:participant, addresses: [old_address, new_address])
-    screening = FactoryBot.create(:screening, participants: [marge])
-    stub_request(:get, intake_api_url(ExternalRoutes.intake_api_screening_path(screening.id)))
-      .and_return(json_body(screening.to_json, status: 200))
-    stub_empty_relationships
-    stub_empty_history_for_screening(screening)
-    visit edit_screening_path(id: screening.id)
-    within edit_participant_card_selector(marge.id) do
-      within all('.list-item').first do
-        expect(page).to have_field('Address', with: '2 new st')
-        expect(page).to have_field('City', with: 'New Springfield')
-      end
-      within all('.list-item').last do
-        expect(page).to have_field('Address', with: '1 old st')
-        expect(page).to have_field('City', with: 'Old Springfield')
-      end
-    end
-  end
-
   scenario 'adding a new address to a participant' do
     visit edit_screening_path(id: screening[:id])
 
