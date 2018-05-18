92ca8504cee452008792811e43c5abbac0d2fba5
Will Mitchell <will.mitchell@osi.ca.gov>
2018-05-18 11:59:13 -0700

    Display SSB values passed in as props

--- app/javascript/views/people/SafelySurrenderedBabyShow.jsx
+++ app/javascript/views/people/SafelySurrenderedBabyShow.jsx
@@ -1,42 +1,61 @@
+import PropTypes from 'prop-types'
 import React from 'react'
 import ShowField from 'common/ShowField'
 
-const SafelySurrenderedBabyShow = () => (
+const SafelySurrenderedBabyShow = ({
+  braceletId,
+  comments,
+  medQuestionaireReturnDate,
+  parentGuardGivenBraceletId,
+  parentGuardProvMedicalQuestionaire,
+  relationToChild,
+  surrenderedBy,
+}) => (
   <div>
     <div>hello</div>
     <div className='row'>
       <ShowField
         label='Surrendered By'
         gridClassName='col-md-4'
-      >Foo!</ShowField>
+      >{surrenderedBy}</ShowField>
       <ShowField
         label='Relationship to Surrendered Child'
         gridClassName='col-md-4'
-      >Foo!</ShowField>
+      >{relationToChild}</ShowField>
       <ShowField
         label='Bracelet ID'
         gridClassName='col-md-4'
-      >Foo!</ShowField>
+      >{braceletId}</ShowField>
 
       <ShowField
         label='Comments'
         gridClassName='col-md-12'
-      >Foo!</ShowField>
+      >{comments}</ShowField>
 
       <ShowField
         label='Parent/Guardian Given Bracelet ID'
         gridClassName='col-md-4'
-      >Foo!</ShowField>
+      >{parentGuardGivenBraceletId ? 'Yes' : 'No'}</ShowField>
       <ShowField
         label='Parent/Guardian Provided Medical Questionaire'
         gridClassName='col-md-4'
-      >Foo!</ShowField>
+      >{parentGuardProvMedicalQuestionaire ? 'Yes' : 'No'}</ShowField>
       <ShowField
         label='Medical Questionaire Return Date'
         gridClassName='col-md-4'
-      >Foo!</ShowField>
+      >{medQuestionaireReturnDate}</ShowField>
     </div>
   </div>
 )
+
+SafelySurrenderedBabyShow.propTypes = {
+  braceletId: PropTypes.string,
+  comments: PropTypes.string,
+  medQuestionaireReturnDate: PropTypes.string,
+  parentGuardGivenBraceletId: PropTypes.bool,
+  parentGuardProvMedicalQuestionaire: PropTypes.bool,
+  relationToChild: PropTypes.string,
+  surrenderedBy: PropTypes.string,
+}
 
 export default SafelySurrenderedBabyShow
--- spec/javascripts/views/people/SafelySurrenderedBabyShowSpec.jsx
+++ spec/javascripts/views/people/SafelySurrenderedBabyShowSpec.jsx
@@ -3,13 +3,21 @@ import {shallow} from 'enzyme'
 import SafelySurrenderedBabyShow from 'views/people/SafelySurrenderedBabyShow'
 
 describe('SafelySurrenderedBabyShow', () => {
-  const render = () => shallow(<SafelySurrenderedBabyShow />)
+  const render = (props) => shallow(<SafelySurrenderedBabyShow {...props}/>)
   const findField = (root, label) => root.find(`ShowField[label="${label}"]`)
 
   let root
 
   beforeEach(() => {
-    root = render()
+    root = render({
+      surrenderedBy: 'Hagrid',
+      relationToChild: 'Groundskeeper',
+      braceletId: 'Lightning',
+      parentGuardGivenBraceletId: true,
+      parentGuardProvMedicalQuestionaire: false,
+      comments: 'Yer a wizard, Harry!',
+      medQuestionaireReturnDate: '2001-11-14',
+    })
   })
 
   it('renders hello', () => {
@@ -24,50 +32,49 @@ describe('SafelySurrenderedBabyShow', () => {
     })
 
     it('is a row', () => {
-      expect(body.exists()).toEqual(true)
       expect(body.props().className).toContain('row')
     })
 
     it('renders Surrendered By', () => {
-      const field = findField(body, 'Surrendered By')
-      expect(field.exists()).toBe(true)
-      expect(field.props().gridClassName).toEqual('col-md-4')
+      const props = findField(body, 'Surrendered By').props()
+      expect(props.gridClassName).toEqual('col-md-4')
+      expect(props.children).toEqual('Hagrid')
     })
 
     it('renders Relationship to Surrendered Child', () => {
-      const field = findField(body, 'Relationship to Surrendered Child')
-      expect(field.exists()).toBe(true)
-      expect(field.props().gridClassName).toEqual('col-md-4')
+      const props = findField(body, 'Relationship to Surrendered Child').props()
+      expect(props.gridClassName).toEqual('col-md-4')
+      expect(props.children).toEqual('Groundskeeper')
     })
 
     it('renders Bracelet ID', () => {
-      const field = findField(body, 'Bracelet ID')
-      expect(field.exists()).toBe(true)
-      expect(field.props().gridClassName).toEqual('col-md-4')
+      const props = findField(body, 'Bracelet ID').props()
+      expect(props.gridClassName).toEqual('col-md-4')
+      expect(props.children).toEqual('Lightning')
     })
 
     it('renders Comments', () => {
-      const field = findField(body, 'Comments')
-      expect(field.exists()).toBe(true)
-      expect(field.props().gridClassName).toEqual('col-md-12')
+      const props = findField(body, 'Comments').props()
+      expect(props.gridClassName).toEqual('col-md-12')
+      expect(props.children).toEqual('Yer a wizard, Harry!')
     })
 
     it('renders Parent/Guardian Given Bracelet ID', () => {
-      const field = findField(body, 'Parent/Guardian Given Bracelet ID')
-      expect(field.exists()).toBe(true)
-      expect(field.props().gridClassName).toEqual('col-md-4')
+      const props = findField(body, 'Parent/Guardian Given Bracelet ID').props()
+      expect(props.gridClassName).toEqual('col-md-4')
+      expect(props.children).toEqual('Yes')
     })
 
     it('renders Parent/Guardian Provided Medical Questionaire', () => {
-      const field = findField(body, 'Parent/Guardian Provided Medical Questionaire')
-      expect(field.exists()).toBe(true)
-      expect(field.props().gridClassName).toEqual('col-md-4')
+      const props = findField(body, 'Parent/Guardian Provided Medical Questionaire').props()
+      expect(props.gridClassName).toEqual('col-md-4')
+      expect(props.children).toEqual('No')
     })
 
     it('renders Medical Questionaire Return Date', () => {
-      const field = findField(body, 'Medical Questionaire Return Date')
-      expect(field.exists()).toBe(true)
-      expect(field.props().gridClassName).toEqual('col-md-4')
+      const props = findField(body, 'Medical Questionaire Return Date').props()
+      expect(props.gridClassName).toEqual('col-md-4')
+      expect(props.children).toEqual('2001-11-14')
     })
   })
 })
