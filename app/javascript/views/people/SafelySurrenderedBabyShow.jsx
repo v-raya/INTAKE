65bf5fe085fc9321f7677c6d67d45513e278ee5f
Will Mitchell <will.mitchell@osi.ca.gov>
2018-05-18 10:50:26 -0700

    Display SSB show fields in bootstrap cols
    This adds 7 show fields to the SafelySurrenderedBabyShow component
    and wraps them in a bootstrap row to match the designs.

--- app/javascript/views/people/SafelySurrenderedBabyShow.jsx
+++ app/javascript/views/people/SafelySurrenderedBabyShow.jsx
@@ -4,13 +4,38 @@ import ShowField from 'common/ShowField'
 const SafelySurrenderedBabyShow = () => (
   <div>
     <div>hello</div>
-    <ShowField
-      errors={[]}
-      required={true}
-      label='Name'
-    >
-      Foo!
-    </ShowField>
+    <div className='row'>
+      <ShowField
+        label='Surrendered By'
+        gridClassName='col-md-4'
+      >Foo!</ShowField>
+      <ShowField
+        label='Relationship to Surrendered Child'
+        gridClassName='col-md-4'
+      >Foo!</ShowField>
+      <ShowField
+        label='Bracelet ID'
+        gridClassName='col-md-4'
+      >Foo!</ShowField>
+
+      <ShowField
+        label='Comments'
+        gridClassName='col-md-12'
+      >Foo!</ShowField>
+
+      <ShowField
+        label='Parent/Guardian Given Bracelet ID'
+        gridClassName='col-md-4'
+      >Foo!</ShowField>
+      <ShowField
+        label='Parent/Guardian Provided Medical Questionaire'
+        gridClassName='col-md-4'
+      >Foo!</ShowField>
+      <ShowField
+        label='Medical Questionaire Return Date'
+        gridClassName='col-md-4'
+      >Foo!</ShowField>
+    </div>
   </div>
 )
 
--- spec/javascripts/views/people/SafelySurrenderedBabyShowSpec.jsx
+++ spec/javascripts/views/people/SafelySurrenderedBabyShowSpec.jsx
@@ -4,14 +4,70 @@ import SafelySurrenderedBabyShow from 'views/people/SafelySurrenderedBabyShow'
 
 describe('SafelySurrenderedBabyShow', () => {
   const render = () => shallow(<SafelySurrenderedBabyShow />)
+  const findField = (root, label) => root.find(`ShowField[label="${label}"]`)
 
-  it('renders hello', () => {
-    const view = render()
-    expect(view.html()).toContain('<div>hello</div>')
+  let root
+
+  beforeEach(() => {
+    root = render()
   })
 
-  it('renders a show field', () => {
-    const view = render()
-    expect(view.find('ShowField').exists()).toBe(true)
+  it('renders hello', () => {
+    expect(root.html()).toContain('<div>hello</div>')
+  })
+
+  describe('body', () => {
+    let body
+
+    beforeEach(() => {
+      body = root.find('.row')
+    })
+
+    it('is a row', () => {
+      expect(body.exists()).toEqual(true)
+      expect(body.props().className).toContain('row')
+    })
+
+    it('renders Surrendered By', () => {
+      const field = findField(body, 'Surrendered By')
+      expect(field.exists()).toBe(true)
+      expect(field.props().gridClassName).toEqual('col-md-4')
+    })
+
+    it('renders Relationship to Surrendered Child', () => {
+      const field = findField(body, 'Relationship to Surrendered Child')
+      expect(field.exists()).toBe(true)
+      expect(field.props().gridClassName).toEqual('col-md-4')
+    })
+
+    it('renders Bracelet ID', () => {
+      const field = findField(body, 'Bracelet ID')
+      expect(field.exists()).toBe(true)
+      expect(field.props().gridClassName).toEqual('col-md-4')
+    })
+
+    it('renders Comments', () => {
+      const field = findField(body, 'Comments')
+      expect(field.exists()).toBe(true)
+      expect(field.props().gridClassName).toEqual('col-md-12')
+    })
+
+    it('renders Parent/Guardian Given Bracelet ID', () => {
+      const field = findField(body, 'Parent/Guardian Given Bracelet ID')
+      expect(field.exists()).toBe(true)
+      expect(field.props().gridClassName).toEqual('col-md-4')
+    })
+
+    it('renders Parent/Guardian Provided Medical Questionaire', () => {
+      const field = findField(body, 'Parent/Guardian Provided Medical Questionaire')
+      expect(field.exists()).toBe(true)
+      expect(field.props().gridClassName).toEqual('col-md-4')
+    })
+
+    it('renders Medical Questionaire Return Date', () => {
+      const field = findField(body, 'Medical Questionaire Return Date')
+      expect(field.exists()).toBe(true)
+      expect(field.props().gridClassName).toEqual('col-md-4')
+    })
   })
 })
