f90abeacd74d822247b0d65fe7b1973924e824cb
manish pokharel <manish.pokharel@osi.ca.gov>
2018-05-18 10:13:55 -0700

    HOT-1836 Added report type dropdown and values in screening information form

--- app/javascript/containers/screenings/ScreeningInformationFormContainer.js
+++ app/javascript/containers/screenings/ScreeningInformationFormContainer.js
@@ -1,6 +1,7 @@
 import {connect} from 'react-redux'
 import ScreeningInformationForm from 'views/ScreeningInformationForm'
 import COMMUNICATION_METHOD from 'enums/CommunicationMethod'
+import REPORT_TYPE from 'enums/ReportType'
 import {setField, touchField, touchAllFields} from 'actions/screeningInformationFormActions'
 import {
   getScreeningInformationFormSelector,
@@ -17,6 +18,8 @@ const mapStateToProps = (state) => {
   const screeningInformationForm = getScreeningInformationFormSelector(state)
   const communicationMethods = Object.keys(COMMUNICATION_METHOD)
     .map((value) => ({value, label: COMMUNICATION_METHOD[value]}))
+  const reportTypes = Object.keys(REPORT_TYPE)
+    .map((value) => ({value, label: REPORT_TYPE[value]}))
   return {
     assignee: screeningInformationForm.getIn(['assignee', 'value']),
     assigneeDisabled: Boolean(screening.get('assignee_staff_id')),
@@ -25,6 +28,8 @@ const mapStateToProps = (state) => {
     endedAt: screeningInformationForm.getIn(['ended_at', 'value']),
     errors: getVisibleErrorsSelector(state).toJS(),
     name: screeningInformationForm.getIn(['name', 'value']),
+    reportType: screeningInformationForm.getIn(['report_type', 'value']),
+    reportTypes,
     startedAt: screeningInformationForm.getIn(['started_at', 'value']),
   }
 }
