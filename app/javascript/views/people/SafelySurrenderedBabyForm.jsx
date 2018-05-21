8ffdf8f4d75b83a951035897a559ff843cac97c1
Will Mitchell <will.mitchell@osi.ca.gov>
2018-05-21 08:38:00 -0700

    Display SSB fields with passed in values

--- app/javascript/views/people/SafelySurrenderedBabyForm.jsx
+++ app/javascript/views/people/SafelySurrenderedBabyForm.jsx
@@ -1,10 +1,91 @@
+import PropTypes from 'prop-types'
 import React from 'react'
+import DateField from 'common/DateField'
 import GrouperHeading from 'common/GrouperHeading'
+import InputField from 'common/InputField'
+import SelectField from 'common/SelectField'
 
-const SafeleySurrenderedBabyForm = () => (
+const SafelySurrenderedBabyForm = ({
+  braceletId,
+  comments,
+  medQuestionaireReturnDate,
+  parentGuardGivenBraceletId,
+  parentGuardProvMedicalQuestionaire,
+  relationToChild,
+  surrenderedBy,
+}) => (
   <div>
     <GrouperHeading text='Safely Surrendered Baby Information' />
+    <div className='row'>
+      <SelectField
+        gridClassName='col-md-4'
+        id='surrendered-by'
+        label='Surrendered By'
+        value={surrenderedBy}
+        onChange={() => {}}
+      >
+        <option key='' value='' />
+      </SelectField>
+      <SelectField
+        gridClassName='col-md-4'
+        id='relation-to-child'
+        label='Relationship to Surrendered Child'
+        value={relationToChild}
+        onChange={() => {}}
+      >
+        <option key='' value='' />
+      </SelectField>
+      <InputField
+        gridClassName='col-md-4'
+        id='bracelet-id'
+        label='Bracelet ID'
+        value={braceletId}
+        onChange={() => {}}
+      />
+      <InputField
+        gridClassName='col-md-12'
+        id='comments'
+        label='Comments'
+        value={comments}
+        onChange={() => {}}
+      />
+      <SelectField
+        gridClassName='col-md-4'
+        id='pg-given-bracelet-id'
+        label='Parent/Guardian Given Bracelet ID'
+        value={parentGuardGivenBraceletId ? 'Yes' : 'No'}
+        onChange={() => {}}
+      >
+        <option key='' value='' />
+      </SelectField>
+      <SelectField
+        gridClassName='col-md-4'
+        id='pg-provided-medical-questionaire'
+        label='Parent/Guardian Provided Medical Questionaire'
+        value={parentGuardProvMedicalQuestionaire ? 'Yes' : 'No'}
+        onChange={() => {}}
+      >
+        <option key='' value='' />
+      </SelectField>
+      <DateField
+        gridClassName='col-md-4'
+        id='med-questionaire-return-date'
+        label='Medical Questionaire Return Date'
+        value={medQuestionaireReturnDate}
+        onChange={() => {}}
+      />
+    </div>
   </div>
 )
 
-export default SafeleySurrenderedBabyForm
+SafelySurrenderedBabyForm.propTypes = {
+  braceletId: PropTypes.string,
+  comments: PropTypes.string,
+  medQuestionaireReturnDate: PropTypes.string,
+  parentGuardGivenBraceletId: PropTypes.bool,
+  parentGuardProvMedicalQuestionaire: PropTypes.bool,
+  relationToChild: PropTypes.string,
+  surrenderedBy: PropTypes.string,
+}
+
+export default SafelySurrenderedBabyForm
--- spec/javascripts/views/people/SafelySurrenderedBabyFormSpec.jsx
+++ spec/javascripts/views/people/SafelySurrenderedBabyFormSpec.jsx
@@ -23,4 +23,58 @@ describe('SafeleySurrenderedBabyForm', () => {
     const header = root.find('GrouperHeading')
     expect(header.props().text).toEqual('Safely Surrendered Baby Information')
   })
+
+  describe('body', () => {
+    let body
+
+    beforeEach(() => {
+      body = root.find('.row')
+    })
+
+    it('is a row', () => {
+      expect(body.props().className).toContain('row')
+    })
+
+    it('renders Surrendered By', () => {
+      const props = body.find('SelectField[label="Surrendered By"]').props()
+      expect(props.gridClassName).toEqual('col-md-4')
+      expect(props.value).toEqual('Hagrid')
+    })
+
+    it('renders Relationship to Surrendered Child', () => {
+      const props = body.find('SelectField[label="Relationship to Surrendered Child"]').props()
+      expect(props.gridClassName).toEqual('col-md-4')
+      expect(props.value).toEqual('Groundskeeper')
+    })
+
+    it('renders Bracelet ID', () => {
+      const props = body.find('InputField[label="Bracelet ID"]').props()
+      expect(props.gridClassName).toEqual('col-md-4')
+      expect(props.value).toEqual('Lightning')
+    })
+
+    it('renders Comments', () => {
+      const props = body.find('InputField[label="Comments"]').props()
+      expect(props.gridClassName).toEqual('col-md-12')
+      expect(props.value).toEqual('Yer a wizard, Harry!')
+    })
+
+    it('renders Parent/Guardian Given Bracelet ID', () => {
+      const props = body.find('SelectField[label="Parent/Guardian Given Bracelet ID"]').props()
+      expect(props.gridClassName).toEqual('col-md-4')
+      expect(props.value).toEqual('Yes')
+    })
+
+    it('renders Parent/Guardian Provided Medical Questionaire', () => {
+      const props = body.find('SelectField[label="Parent/Guardian Provided Medical Questionaire"]').props()
+      expect(props.gridClassName).toEqual('col-md-4')
+      expect(props.value).toEqual('No')
+    })
+
+    it('renders Medical Questionaire Return Date', () => {
+      const props = body.find('DateField[label="Medical Questionaire Return Date"]').props()
+      expect(props.gridClassName).toEqual('col-md-4')
+      expect(props.value).toEqual('2001-11-14')
+    })
+  })
 })
