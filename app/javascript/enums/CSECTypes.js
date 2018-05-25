6a79371723ef15db12098aad9edf8479e8f7349a
Naga Pradeep Gajula <naga.gajula@osi.ca.gov>
2018-05-25 12:38:41 -0700

    HOT-1841 Add csec type values to enums

---
+++ app/javascript/enums/CSECTypes.js
@@ -0,0 +1,11 @@
+const CSEC_TYPES = Object.freeze([
+  'At Risk',
+  'Victim Before Foster Care',
+  'Victim During Foster Care',
+  'Victim in Open Case not in Foster Care',
+  'Victim while Absent from Placement',
+  'Victim with Closed Case',
+  'Rcv ILP Svcs',
+])
+
+export default CSEC_TYPES
