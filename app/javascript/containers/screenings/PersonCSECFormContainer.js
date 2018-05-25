26ae13b5fdf4802a7d689b3968b7022abbfe2963
Naga Pradeep Gajula <naga.gajula@osi.ca.gov>
2018-05-25 12:46:29 -0700

    HOT-1841 Add checks for CSEC to show only when report type and roles are satisfied

---
+++ app/javascript/containers/screenings/PersonCSECFormContainer.js
@@ -0,0 +1,21 @@
+import {connect} from 'react-redux'
+import {
+  getPersonCSECDetailsSelector,
+  getCSECRequireValidationSelector,
+  getVisibleErrorsSelector,
+} from 'selectors/screening/perosnCSECFormSelectors'
+import {setField, touchField} from 'actions/peopleFormActions'
+import PersonCSECForm from 'views/people/PersonCSECForm'
+
+const mapStateToProps = (state, {personId}) => ({
+  ...getPersonCSECDetailsSelector(state, personId).toJS(),
+  showCSEC: getCSECRequireValidationSelector(state, personId),
+  errors: getVisibleErrorsSelector(state, personId).toJS(),
+})
+
+const mapDispatchToProps = (dispatch, {personId}) => ({
+  onBlur: (field) => dispatch(touchField(personId, [field])),
+  onChange: (field, value) => dispatch(setField(personId, [field], value)),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(PersonCSECForm)
