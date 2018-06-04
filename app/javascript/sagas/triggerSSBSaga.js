63de9f6bec21aaa8fed06ceee33d59dd2346d786
Will Mitchell <will.mitchell@osi.ca.gov>
2018-06-04 16:02:48 -0700

    Put participant success and create allegation

--- app/javascript/sagas/triggerSSBSaga.js
+++ app/javascript/sagas/triggerSSBSaga.js
@@ -1,24 +1,43 @@
-import {takeEvery, call, select} from 'redux-saga/effects'
+import {List, Map} from 'immutable'
+import {takeEvery, call, select, put} from 'redux-saga/effects'
+import {createPersonSuccess} from 'actions/personCardActions'
+import {saveSuccess} from 'actions/screeningActions'
 import {babyDoe, parentDoe} from 'data/participants'
 import {getScreeningSelector} from 'selectors/screeningSelectors'
-import {post} from 'utils/http'
+import {post, put as putHTTP} from 'utils/http'
 
 export function* triggerSSB({payload: screening_id}) {
   try {
-    yield call(post, '/api/v1/participants', {
+    const baby = yield call(post, '/api/v1/participants', {
       participant: {
         screening_id,
         ...babyDoe,
       },
     })
-    yield call(post, '/api/v1/participants', {
+    yield put(createPersonSuccess(baby))
+    const caretaker = yield call(post, '/api/v1/participants', {
       participant: {
         screening_id,
         ...parentDoe,
       },
     })
+    yield put(createPersonSuccess(caretaker))
 
-    yield select(getScreeningSelector)
+    const screening = yield select(getScreeningSelector)
+    const screeningWithAllegation = screening.set('allegations',
+      screening.get('allegations', List()).push(Map({
+        screening_id,
+        victim_person_id: baby.id,
+        perpetrator_person_id: caretaker.id,
+        types: ['Caretaker absent/incapacity'],
+      }))
+    )
+
+    const path = `/api/v1/screenings/${screening_id}`
+    const response = yield call(putHTTP, path, {
+      screening: screeningWithAllegation.toJS(),
+    })
+    yield put(saveSuccess(response))
   } catch (error) {
     console.log('Error')
   }
--- spec/javascripts/sagas/triggerSSBSagaSpec.js
+++ spec/javascripts/sagas/triggerSSBSagaSpec.js
@@ -1,11 +1,14 @@
-import {takeEvery, call, select} from 'redux-saga/effects'
+import {fromJS} from 'immutable'
+import {takeEvery, call, select, put} from 'redux-saga/effects'
+import {createPersonSuccess} from 'actions/personCardActions'
+import {saveSuccess} from 'actions/screeningActions'
 import {babyDoe, parentDoe} from 'data/participants'
 import {
   triggerSSB,
   triggerSSBSaga,
 } from 'sagas/triggerSSBSaga'
 import {getScreeningSelector} from 'selectors/screeningSelectors'
-import {post} from 'utils/http'
+import {post, put as putHTTP} from 'utils/http'
 
 describe('triggerSSBSaga', () => {
   it('triggers SSB info creation on TRIGGER_SSB', () => {
@@ -26,13 +29,46 @@ describe('triggerSSB', () => {
       },
     }))
 
-    expect(gen.next({id: '111'}).value).toEqual(call(post, '/api/v1/participants', {
+    const babyResponse = {id: '111'}
+    const putBaby = gen.next(babyResponse)
+
+    expect(putBaby.value).toEqual(put(createPersonSuccess(babyResponse)))
+
+    expect(gen.next().value).toEqual(call(post, '/api/v1/participants', {
       participant: {
         screening_id,
         ...parentDoe,
       },
     }))
 
-    expect(gen.next({id: '222'}).value).toEqual(select(getScreeningSelector))
+    const caretakerResponse = {id: '222'}
+    const putCaretaker = gen.next(caretakerResponse)
+
+    expect(putCaretaker.value).toEqual(put(createPersonSuccess(caretakerResponse)))
+
+    expect(gen.next().value).toEqual(select(getScreeningSelector))
+
+    const screening = {
+      participants: [babyDoe, parentDoe],
+    }
+
+    const allegation = {
+      screening_id,
+      victim_person_id: '111',
+      perpetrator_person_id: '222',
+      types: ['Caretaker absent/incapacity'],
+    }
+
+    expect(gen.next(fromJS(screening)).value).toEqual(call(putHTTP, '/api/v1/screenings/1', {
+      screening: {
+        ...screening,
+        participants: [babyDoe, parentDoe],
+        allegations: [allegation],
+      },
+    }))
+
+    const screeningResponse = 'New Screening'
+    const putScreening = gen.next(screeningResponse)
+    expect(putScreening.value).toEqual(put(saveSuccess(screeningResponse)))
   })
 })
