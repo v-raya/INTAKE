0ba307d27855f7f65d33f01397d7d1ad9790a97a
amanbop <aman_boparai@yahoo.com>
2018-06-04 17:00:57 -0700

    Added new-key to avoid duplicate key values

--- app/javascript/common/Relationships.jsx
+++ app/javascript/common/Relationships.jsx
@@ -14,7 +14,7 @@ export const Relationships = ({people, onClick, screeningId, isScreening, pendin
   <div className='card-body no-pad-top'>
     {
       people.map((person, index) => (
-        <div className='row' key={index}>
+        <div className='row' key={`new-${index}`}>
           <div className='col-md-12'>
             {
               (person.relationships.length > 0) &&
