c8ac23d143ec6b801195d77033326721730ab009
amanbop <aman_boparai@yahoo.com>
2018-06-08 09:19:46 -0700

    1.Added conditional logic for separating out Relationships for Screening and Snapshots 2.Added Tests for Relationships for Snapshot with conditional logic

--- spec/javascripts/components/common/RelationshipsSpec.jsx
+++ spec/javascripts/components/common/RelationshipsSpec.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import {mount} from 'enzyme'
-import {Relationships} from 'common/Relationships'
+import {shallow,mount} from 'enzyme'
+import {EmptyRelationships,Relationships} from 'common/Relationships'
 
-describe('Relationships', () => {
+describe('Relationships for Screening', () => {
   const onClick = jasmine.createSpy('onClick')
   const renderRelationships = (props) => mount(<Relationships {...props} isScreening={true} screeningId={'1'} pendingPeople = {['1']} onClick={onClick} />, {disableLifecycleMethods: true})
   const people = [
@@ -114,3 +114,94 @@ describe('Relationships', () => {
     expect(component.find('.no-relationships').at(2).text()).toContain('Kim West has no known relationships')
   })
 })
+
+describe('Relationships for Snapshot', () => {
+  const onClick = jasmine.createSpy('onClick')
+  const renderRelationships = (props) => shallow(<Relationships {...props} isScreening={false} onClick={onClick} />, {disableLifecycleMethods: true})
+  it('renders people with no relationships', () => {
+    const people = [
+      {name: 'Sally Jones', relationships: []},
+      {name: 'Nate Starbringer', relationships: []},
+      {name: 'Jim Johnson', relationships: []},
+    ]
+    const component = renderRelationships({people})
+    expect(component.find('.person').at(0).text()).toEqual('Sally Jones')
+    expect(component.find('.person').at(1).text()).toEqual('Nate Starbringer')
+    expect(component.find('.person').at(2).text()).toEqual('Jim Johnson')
+  })
+
+  it('renders relationships for each person', () => {
+    const people = [
+      {
+        name: 'Sally Jones',
+        relationships: [
+          {relatee: 'Jim Johnson', type: 'mother', person_card_exists: true},
+        ],
+      },
+      {
+        name: 'Nate Starbringer',
+        relationships: [
+          {relatee: 'Jim Johnson', type: 'father', person_card_exists: false},
+        ],
+      },
+      {
+        name: 'Jim Johnson',
+        relationships: [
+          {relatee: 'Nate Starbringer', type: 'son', person_card_exists: true},
+          {relatee: 'Sally Jones', type: 'son', person_card_exists: true},
+        ],
+      },
+    ]
+    const component = renderRelationships({people})
+    expect(component.find('.relationships').at(0).find('li').at(0).text()).toEqual('mother of Jim Johnson Attach')
+    expect(component.find('.relationships').at(1).find('li').at(0).text()).toEqual('father of Jim Johnson')
+    expect(component.find('.relationships').at(2).find('li').at(1).text()).toEqual('son of Sally Jones Attach')
+    expect(component.find('.relationships').at(2).find('li').at(0).text()).toEqual('son of Nate Starbringer Attach')
+  })
+
+  it('hides Attach link for people in the pending list', () => {
+    const people = [
+      {
+        name: 'Sally Jones',
+        relationships: [
+          {relatee: 'Jim Johnson', type: 'mother', person_card_exists: true, legacy_descriptor: {legacy_id: '1'}},
+        ],
+      },
+      {
+        name: 'Nate Starbringer',
+        relationships: [
+          {relatee: 'Jim Johnson', type: 'father', person_card_exists: true},
+        ],
+      },
+    ]
+
+    const pendingPeople = ['1']
+
+    const component = renderRelationships({people, pendingPeople})
+    expect(component.find('.relationships').at(0).find('li').at(0).text()).toEqual('mother of Jim Johnson')
+    expect(component.find('.relationships').at(1).find('li').at(0).text()).toEqual('father of Jim Johnson Attach')
+  })
+
+  it('calls onClick when the Attach Link is clicked', () => {
+    const people = [
+      {
+        name: 'Goku',
+        relationships: [
+          {relatee: 'Gohan', type: 'son', person_card_exists: true},
+        ],
+      },
+    ]
+    const component = renderRelationships({people})
+    const attachLink = component.find('a').at(0)
+    attachLink.simulate('click')
+    expect(onClick).toHaveBeenCalled()
+  })
+})
+
+describe('EmptyRelationships', () => {
+  it('renders a reminder to add people when there are no relationships', () => {
+    const component = shallow(<EmptyRelationships />, {disableLifecycleMethods: true})
+    expect(component.find('.empty-relationships').text()).toEqual('Search for people and add them to see their relationships.')
+  })
+})
+
