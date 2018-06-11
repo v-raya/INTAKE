bf1a9cc868552fb5bbbf9d7fbde8ac294f6237b4
Tyler Clemens <Tyler.Clemens@osi.ca.gov>
2018-06-11 10:35:15 -0700

    Update Race label on person card form to indicate primary first
    [HOT-1474]

--- app/javascript/views/people/RaceForm.jsx
+++ app/javascript/views/people/RaceForm.jsx
@@ -15,7 +15,7 @@ const RaceForm = ({
     <div className='col-md-12 gap-top'>
       <div className='gap-top' id='race'>
         <fieldset className='fieldset-inputs'>
-          <label>Race</label>
+          <label>Race (Primary First)</label>
           <div className='row'>
             <div className='col-md-6'>
               <ul className='unstyled-list'>
