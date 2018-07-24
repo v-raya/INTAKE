3a9263f16b524f30f14af40b3b0a8a6d6a1ba57e
karthik-sista <karthik.sista@osi.ca.gov>
2018-07-24 14:26:54 -0700

    Added logic to sort the blank DOB to last when ascending and viceversa.

--- app/javascript/utils/sortDateOfBirth.js
+++ app/javascript/utils/sortDateOfBirth.js
@@ -1,6 +1,37 @@
+const SORT_DOWN = 1
+const SORT_UP = -1
+
 export const sortDateOfBirth = (a, b, order) => {
-  var aDOB = a.dateOfBirth || new Date()
-  var bDOB = b.dateOfBirth || new Date()
+  if (a.dateOfBirth === '' && b.dateOfBirth === '') {
+    return 0
+  }
+  // } else if (a.dateOfBirth === '' && b.dateOfBirth !== '' && order === 'asc') {
+  //   return SORT_UP
+  // } else if (b.dateOfBirth === '' && a.dateOfBirth !== '' && order === 'asc') {
+  //   return SORT_DOWN
+  // } else if (a.dateOfBirth === '' && b.dateOfBirth !== '' && order === 'desc') {
+  //   return SORT_DOWN
+  // } else if (b.dateOfBirth === '' && a.dateOfBirth !== '' && order === 'desc') {
+  //   return SORT_UP
+  // }
+
+  if (order === 'desc') {
+    if (a.dateOfBirth === '') {
+      return SORT_DOWN
+    }
+    if (b.dateOfBirth === '') {
+      return SORT_UP
+    }
+  } else {
+    if (a.dateOfBirth === '') {
+      return SORT_UP
+    }
+    if (b.dateOfBirth === '') {
+      return SORT_DOWN
+    }
+  }
+  var aDOB = a.dateOfBirth
+  var bDOB = b.dateOfBirth
   var dateObject1 = new Date(aDOB)
   var dateObject2 = new Date(bDOB)
   if (order === 'desc') {
--- spec/javascripts/utils/sortDateOfBirthSpec.js
+++ spec/javascripts/utils/sortDateOfBirthSpec.js
@@ -1,5 +1,8 @@
 import {sortDateOfBirth} from 'utils/sortDateOfBirth'
 
+const ascOrder = 'asc'
+const descOrder = 'desc'
+
 describe('Sort Date Of Birth', () => {
   const a = {
     dateOfBirth: '10/29/1992',
@@ -9,14 +12,61 @@ describe('Sort Date Of Birth', () => {
     dateOfBirth: '2/23/1995',
   }
 
-  const asccorder = 'asc'
-  const descorder = 'desc'
-
   it('sorts by ascending order and return a negative value', () => {
-    expect(sortDateOfBirth(a, b, asccorder) < 0).toBe(true)
+    expect(sortDateOfBirth(a, b, ascOrder) < 0).toBe(true)
   })
 
   it('sorts by descending order and return a positive value', () => {
-    expect(sortDateOfBirth(a, b, descorder) > 0).toBe(true)
+    expect(sortDateOfBirth(a, b, descOrder) > 0).toBe(true)
+  })
+})
+
+describe('Sort Date Of Birth if DOB is empty string', () => {
+  const a = {
+    dateOfBirth: '',
+  }
+
+  const b = {
+    dateOfBirth: '2/23/1995',
+  }
+
+  const c = {
+    dateOfBirth: '2/23/1995',
+  }
+
+  const d = {
+    dateOfBirth: '',
+  }
+
+  it('sorts by ascending order and return a negative value', () => {
+    expect(sortDateOfBirth(a, b, ascOrder) < 0).toBe(true)
+  })
+
+  it('sorts by descending order and return a positive value', () => {
+    expect(sortDateOfBirth(a, b, descOrder) > 0).toBe(true)
+  })
+  it('sorts by ascending order and return a positive value', () => {
+    expect(sortDateOfBirth(c, d, ascOrder) > 0).toBe(true)
+  })
+  it('sorts by descending order and return a negative value', () => {
+    expect(sortDateOfBirth(c, d, descOrder) < 0).toBe(true)
+  })
+})
+
+describe('Return 0 if DOBs are empty', () => {
+  const a = {
+    dateOfBirth: '',
+  }
+
+  const b = {
+    dateOfBirth: '',
+  }
+
+  it('sorts by ascending order and return a negative value', () => {
+    expect(sortDateOfBirth(a, b, ascOrder) === 0).toBe(true)
+  })
+
+  it('sorts by descending order and return a positive value', () => {
+    expect(sortDateOfBirth(a, b, descOrder) === 0).toBe(true)
   })
 })
