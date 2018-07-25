04a48d98451fd13f388b60195660af8a28331a41
karthik-sista <karthik.sista@osi.ca.gov>
2018-07-25 09:30:46 -0700

    Test code climate fix.

--- app/javascript/utils/sortDateOfBirth.js
+++ app/javascript/utils/sortDateOfBirth.js
@@ -4,21 +4,14 @@ const SORT_UP = -1
 export const sortDateOfBirth = (a, b, order) => {
   if (a.dateOfBirth === '' && b.dateOfBirth === '') {
     return 0
-  }
-  if (order === 'desc') {
-    if (a.dateOfBirth === '') {
-      return SORT_DOWN
-    }
-    if (b.dateOfBirth === '') {
-      return SORT_UP
-    }
-  } else {
-    if (a.dateOfBirth === '') {
-      return SORT_UP
-    }
-    if (b.dateOfBirth === '') {
-      return SORT_DOWN
-    }
+  } else if (a.dateOfBirth === '' && order === 'asc') {
+    return SORT_UP
+  } else if (b.dateOfBirth === '' && order === 'asc') {
+    return SORT_DOWN
+  } else if (a.dateOfBirth === '' && order === 'desc') {
+    return SORT_DOWN
+  } else if (b.dateOfBirth === '' && order === 'desc') {
+    return SORT_UP
   }
   const dateObject1 = new Date(a.dateOfBirth)
   const dateObject2 = new Date(b.dateOfBirth)
