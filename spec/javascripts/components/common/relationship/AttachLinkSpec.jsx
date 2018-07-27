43de30ce19a78440159050fc0e8d0d7064da2e99
Naga Pradeep Gajula <naga.gajula@osi.ca.gov>
2018-07-27 11:38:36 -0700

    SNAP-411 add aria-label and href so that attachlinks are tabable and readable

--- spec/javascripts/components/common/relationship/AttachLinkSpec.jsx
+++ spec/javascripts/components/common/relationship/AttachLinkSpec.jsx
@@ -47,4 +47,9 @@ describe('AttachLink', () => {
     expect(onClick).toHaveBeenCalled()
     expect(onClick).toHaveBeenCalledWith({name: 'Gohan', type: 'son', person_card_exists: true}, '1')
   })
+
+  it('has href and aria-label', () => {
+    expect(renderAttachLink(props).find('a[aria-label="attachRelationship"]').props().href)
+      .toBe('#')
+  })
 })
