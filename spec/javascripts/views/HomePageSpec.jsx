ce108384d73fe539796ecf49677261c01d0a7a31
Naga Pradeep Gajula <naga.gajula@osi.ca.gov>
2018-08-13 10:05:12 -0700

    HOT-2065 Introduce Breadcrumb to Intake homePage/Caseload

--- spec/javascripts/views/HomePageSpec.jsx
+++ spec/javascripts/views/HomePageSpec.jsx
@@ -66,4 +66,9 @@ describe('HomePage', () => {
       expect(homePage.find('Connect(ScreeningsTable)').length).toBe(1)
     })
   })
+  it('renders a breadCrumb', () => {
+    const props = {snapshot: true, hotline: true, actions: {}}
+    const homePage = shallow(<HomePage {...props}/>)
+    expect(homePage.find('BreadCrumb').exists()).toEqual(true)
+  })
 })
