11304afdcffe6e300df9a627b284bb6c7dd73ea2
Money <manmohit.singh@gmail.com>
2018-08-17 16:17:47 -0700

    Revert functions on const variables. Remove unused props from BootstrapTable

--- app/javascript/screenings/ScreeningsTable.jsx
+++ app/javascript/screenings/ScreeningsTable.jsx
@@ -6,7 +6,7 @@ import moment from 'moment'
 import {Link} from 'react-router'
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table'
 
-function decisionType(cell, row) {
+const decisionType = (cell, row) => {
   if (['promote_to_referral', 'screen_out'].includes(row.screening_decision)) {
     const responseTimes = SCREENING_DECISION_OPTIONS[row.screening_decision]
     return responseTimes.values[row.screening_decision_detail]
@@ -18,22 +18,20 @@ const textWrap = {whiteSpace: 'normal'}
 
 const linkName = (id, referralId, name) => (name || referralId || id)
 
-function screeningNameLink(cell, row) {
-  return <Link to={`/screenings/${row.id}`}>{linkName(row.id, row.referralId, row.name)}</Link>
-}
+const screeningNameLink = (cell, row) => (
+  <Link to={`/screenings/${row.id}`}>{linkName(row.id, row.referralId, row.name)}</Link>
+)
 
-function reportDateAndTime(cell, row) {
-  const startedAt = moment(row.started_at)
-  if (startedAt.isValid()) {
+const reportDateAndTime = (startedAt) => {
+  if (moment(startedAt).isValid()) {
     return (
       <div>
-        {startedAt.format('L LT')} <br/>
-        <em className='text-muted'>({startedAt.fromNow()})</em>
+        {moment(startedAt).format('L LT')} <br/>
+        <em className='text-muted'>({moment(startedAt).fromNow()})</em>
       </div>
     )
-  } else {
-    return undefined
   }
+  return undefined
 }
 
 class ScreeningsTable extends React.Component {
@@ -46,53 +44,16 @@ class ScreeningsTable extends React.Component {
     return (
       <div className='responsive'>
         <BootstrapTable
-          bordered={false}
-          search={false}
-          data={this.props.screenings}
-          searchPlaceholder='Quick Filter'
-          options={{sortName: 'started_at', sortOrder: 'asc'}}
-          tdStyle={textWrap}
+          bordered={false} data={this.props.screenings} options={{sortName: 'started_at', sortOrder: 'asc'}} tdStyle={textWrap}
         >
-          <TableHeaderColumn
-            dataField='name'
-            dataFormat={screeningNameLink}
-            dataSort={true}
-            tdStyle= {textWrap}
-            isKey={true}
-            sortHeaderColumnClassName='sorted-header'
-          >
-            Screening Name
-          </TableHeaderColumn>
-          <TableHeaderColumn
-            dataField='screening_decision'
-            dataFormat={decisionType}
-            dataSort={false}
-          >
-            Type/Decision
-          </TableHeaderColumn>
-          <TableHeaderColumn
-            dataField='screening_status'
-            dataSort={true}
-            sortHeaderColumnClassName='sorted-header'
-          >
-            Status
-          </TableHeaderColumn>
-          <TableHeaderColumn
-            dataField='assignee'
-            tdStyle= {textWrap}
-            dataSort={false}
-          >
-            Assignee
-          </TableHeaderColumn>
-          <TableHeaderColumn
-            dataField='started_at'
-            dataFormat={reportDateAndTime}
-            dataSort={true}
-            sortHeaderColumnClassName='sorted-header'
-          >
-            Report Date <br/>
-            and Time
-          </TableHeaderColumn>
+          <TableHeaderColumn dataField='name' dataFormat={screeningNameLink} dataSort={true} tdStyle= {textWrap} isKey={true}>
+          Screening Name</TableHeaderColumn>
+          <TableHeaderColumn dataField='screening_decision' dataFormat={decisionType} dataSort={false} tdStyle={textWrap}>
+          Type/Decision</TableHeaderColumn>
+          <TableHeaderColumn dataField='screening_status' dataSort={true}>Status</TableHeaderColumn>
+          <TableHeaderColumn dataField='assignee' tdStyle= {textWrap} dataSort={false}>Assignee</TableHeaderColumn>
+          <TableHeaderColumn dataField='started_at' dataFormat={reportDateAndTime} dataSort={true} tdStyle={textWrap}>
+          Report Date <br/>and Time</TableHeaderColumn>
         </BootstrapTable>
       </div>
     )
