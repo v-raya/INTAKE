b86228b61cc5d68fab7c81bfbbc03e3e97cbf133
Will Mitchell <will.mitchell@osi.ca.gov>
2018-08-22 15:12:28 -0700

    Hacky fix for feature test failures
    They were passing locally and only failing in Docker. I am still
    not entirely sure why, but changing the order of which cards are
    saved fixes the problem.

--- spec/features/screening/submit_screening_spec.rb
+++ spec/features/screening/submit_screening_spec.rb
@@ -58,8 +58,8 @@ feature 'Submit Screening' do
       within('.card', text: 'Incident Information') { click_button 'Save' }
       within('.card', text: 'Allegations') { click_button 'Save' }
       within('.card', text: 'Worker Safety') { click_button 'Save' }
-      within('.card', text: 'Cross Report') { click_button 'Save' }
       within('.card', text: 'Decision') { click_button 'Save' }
+      within('.card', text: 'Cross Report') { click_button 'Save' }
       expect(page).to have_button('Submit', disabled: false)
     end
 
@@ -93,9 +93,9 @@ feature 'Submit Screening' do
       within('.card', text: 'Incident Information') { click_button 'Save' }
       within('.card', text: 'Allegations') { click_button 'Save' }
       within('.card', text: 'Worker Safety') { click_button 'Save' }
-      within('.card', text: 'Cross Report') { click_button 'Save' }
-      expect(page).to have_button('Submit', disabled: true)
       within('.card', text: 'Decision') { click_button 'Save' }
+      expect(page).to have_button('Submit', disabled: true)
+      within('.card', text: 'Cross Report') { click_button 'Save' }
       expect(page).to have_button('Submit', disabled: false)
     end
   end
