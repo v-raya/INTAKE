3667982ca8dbaa458575cafe470a8720fc99838d
Melbourne Lipardo <melbourne.lipardo@osi.ca.gov>
2018-08-24 12:54:37 -0700

    Added batch create relationships actions.

--- app/javascript/actions/relationshipsActions.js
+++ app/javascript/actions/relationshipsActions.js
@@ -4,6 +4,25 @@ import {
   CLEAR_RELATIONSHIPS,
 } from 'actions/actionTypes'
 
+export const BATCH_CREATE_RELATIONSHIPS = 'BATCH_CREATE_RELATIONSHIPS'
+export const BATCH_CREATE_RELATIONSHIPS_COMPLETE = 'BATCH_CREATE_RELATIONSHIPS_COMPLETE'
+export const BATCH_CREATE_RELATIONSHIPS_ERROR = 'BATCH_CREATE_RELATIONSHIPS_ERROR'
+
+export const batchCreateRelationships = () => ({
+  type: BATCH_CREATE_RELATIONSHIPS,
+})
+
+export const batchCreateRelationshipsFaliure = (error) => ({
+  type: BATCH_CREATE_RELATIONSHIPS_ERROR,
+  payload: {error},
+  error: true,
+})
+
+export const batchCreateRelationshipsSuccess = (relationships) => ({
+  type: BATCH_CREATE_RELATIONSHIPS_COMPLETE,
+  payload: {relationships},
+})
+
 export function clearRelationships() {
   return {type: CLEAR_RELATIONSHIPS}
 }
