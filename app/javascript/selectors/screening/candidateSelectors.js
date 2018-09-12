220e24f77bfa7f7778988785c8d2fc16f4ad5911
Melbourne Lipardo <melbourne.lipardo@osi.ca.gov>
2018-09-12 09:41:05 -0700

    Removes any candidate when the relationship is null or empty string.
    This removes a candidate when the relationship is empty and when sending the payload to the API.

--- app/javascript/selectors/screening/candidateSelectors.js
+++ app/javascript/selectors/screening/candidateSelectors.js
@@ -1,14 +1,32 @@
 import {List, Map} from 'immutable'
+import {Maybe} from 'utils/maybe'
 
 export const selectCandidates = (state, id) =>
   state.get('candidatesForm').get(id, List())
 
+const isEmptyRelationship = (candidate) =>
+  candidate.get('relationship_type') === ''
+
+const toCode = (candidate) =>
+  Maybe.of(candidate.getIn(['candidate', 'relationshipType']))
+    .map((type) => parseInt(type, 10))
+    .map((code) => {
+      if (isNaN(code)) {
+        return ''
+      } else {
+        return code
+      }
+    })
+    .valueOrElse('')
+
 export const selectCandidatesWithEdits = (state, id) => (Map({
-  relationships: selectCandidates(state, id).map((candidate) => Map({
-    client_id: candidate.getIn(['person', 'id']),
-    relative_id: candidate.getIn(['candidate', 'id']),
-    relationship_type: parseInt(candidate.getIn(['candidate', 'relationshipType']), 10),
-    absent_parent_indicator: false,
-    same_home_status: 'N',
-  })),
+  relationships: selectCandidates(state, id)
+    .map((candidate) => Map({
+      client_id: candidate.getIn(['person', 'id']),
+      relative_id: candidate.getIn(['candidate', 'id']),
+      relationship_type: toCode(candidate),
+      absent_parent_indicator: false,
+      same_home_status: 'N',
+    }))
+    .filterNot(isEmptyRelationship),
 }))
