0fe7f8deecd51bb51f2222a813626e0f776fb381
Tyler Clemens <Tyler.Clemens@osi.ca.gov>
2018-09-13 16:06:15 -0700

    Rescue when there is a problem setting the error and push a process error
    HOT-2530

--- app/javascript/reducers/errorsReducer.js
+++ app/javascript/reducers/errorsReducer.js
@@ -3,18 +3,26 @@ import {CLEAR_ERRORS} from 'actions/clearErrors'
 
 const initialState = fromJS({})
 
+export const PROCESS_ERROR = 'The application encountered an error and could not process it'
+export const PROCESS_TYPE = 'PROCESS_TYPE'
+
 export default function errorsReducer(state = initialState, action) {
-  const {payload, error, type} = action
+  try {
+    const {payload, error, type} = action
 
-  if (error) {
-    const {error: {responseJSON = {}} = {}} = payload
-    const {api_response_body: {issue_details} = {}} = responseJSON
-    return state.set(type, fromJS(issue_details || payload))
+    if (error) {
+      const {error: errorPayload} = payload
+      const {responseJSON} = errorPayload
+      const {api_response_body: {issue_details} = {}} = responseJSON || errorPayload
+      return state.set(type, fromJS(issue_details))
+    }
+
+    if (type === CLEAR_ERRORS) {
+      return initialState
+    }
+
+    return state.delete(type)
+  } catch (error) {
+    return state.set(PROCESS_TYPE, fromJS({error: [PROCESS_ERROR]}))
   }
-
-  if (type === CLEAR_ERRORS) {
-    return initialState
-  }
-
-  return state.delete(type)
 }
