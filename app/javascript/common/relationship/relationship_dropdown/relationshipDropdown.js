9025170c195dee807a7857814537ccf873e10f50
amanbop <aman_boparai@yahoo.com>
2018-09-20 16:17:38 -0700

    Intial Refactor

--- app/javascript/common/relationship/relationship_dropdown/relationshipDropdown.js
+++ app/javascript/common/relationship/relationship_dropdown/relationshipDropdown.js
@@ -1,161 +1,31 @@
 import {RELATIONSHIP_TYPES} from 'enums/RelationshipTypes'
+import moment from 'moment'
 
-import {
-  uniq,
-  isGenderUnknown,
-  getClientBirthDate,
-  doesClientHaveDOB,
-  isPrimaryClientYounger,
-  isSecondaryClientYounger,
-  whenBothHaveDOBandGender,
-  whenBothHaveDOBnoGender,
-  whenBothHaveDOBnoSecndryGender,
-  whenBothHaveDOBnoPrmaryGender,
-} from './helperMethods'
+const MALE_CODES = ['M', 'm', 'U', 'I']
+const FEMALE_CODES = ['F', 'f', 'U', 'I']
+const YOUNGER_CODES = ['f', 'm']
+const PRIMARY = 0
+const SECONDARY = 1
 
-export const doBothHaveDOB = (primaryClient, secondaryClient) => doesClientHaveDOB(primaryClient) && doesClientHaveDOB(secondaryClient)
+const isCompatible = (index, codes, type) => codes.includes(type.gender_code[index])
 
-export const areGenderBothKnown = (primaryClient, secondaryClient) => !isGenderUnknown(primaryClient) && !isGenderUnknown(secondaryClient)
+const isOlder = (clientA, clientB) =>
+  clientA.dateOfBirth && clientB.dateOfBirth &&
+  moment(clientA.dateOfBirth, 'MM/DD/YYYY') < moment(clientB.dateOfBirth, 'MM/DD/YYYY')
 
-export const areGenderBothUnknown = (primaryClient, secondaryClient) => isGenderUnknown(primaryClient) && isGenderUnknown(secondaryClient)
+const areGendersCompatible = (client, index) => (type) =>
+  (isCompatible(index, MALE_CODES, type) || client.gender_code !== 'M') &&
+  (isCompatible(index, FEMALE_CODES, type) || client.gender_code !== 'F')
 
-export const isGenderPriKnownSecUnknown = (primaryClient, secondaryClient) => !isGenderUnknown(primaryClient) && isGenderUnknown(secondaryClient)
+const areAgesCompatible = (primaryClient, secondaryClient) => (type) =>
+  !(isCompatible(PRIMARY, YOUNGER_CODES, type) && isOlder(primaryClient, secondaryClient)) &&
+  !(isCompatible(SECONDARY, YOUNGER_CODES, type) && isOlder(secondaryClient, primaryClient))
 
-export const isGenderPriUnknownSecKnown = (primaryClient, secondaryClient) => isGenderUnknown(primaryClient) && !isGenderUnknown(secondaryClient)
+const relationshipDropdown = (primaryClient, secondaryClient) =>
+  RELATIONSHIP_TYPES
+    .filter(areGendersCompatible(primaryClient, PRIMARY))
+    .filter(areGendersCompatible(secondaryClient, SECONDARY))
+    .filter(areAgesCompatible(primaryClient, secondaryClient))
+    .filter((rec) => !rec.label.match(/\*/))
 
-export const noDOBnoGender = (
-  primaryClient,
-  secondaryClient,
-  genderBothUnknown,
-  genderPriUnknownSecKnown,
-  genderPriKnownSecUnknown
-) => {
-  let genderCodesToReturn = []
-
-  if (genderPriUnknownSecKnown) {
-    genderCodesToReturn.push(
-      `m${secondaryClient.gender_code}`, `f${secondaryClient.gender_code}`, `M${secondaryClient.gender_code}`, `F${secondaryClient.gender_code}`,
-      `M${secondaryClient.gender_code.toLowerCase()}`, `F${secondaryClient.gender_code.toLowerCase()}`, 'FU', 'MU', 'UU')
-  }
-  if (genderPriKnownSecUnknown) {
-    genderCodesToReturn.push(
-      `${primaryClient.gender_code}m`, `${primaryClient.gender_code}f`, `${primaryClient.gender_code}M`, `${primaryClient.gender_code}F`,
-      `${primaryClient.gender_code.toLowerCase()}M`, `${primaryClient.gender_code.toLowerCase()}F`, 'UF', 'UM', 'UU')
-  }
-  if (genderBothUnknown) {
-    genderCodesToReturn = uniq(RELATIONSHIP_TYPES.map((rec) => rec.gender_code))
-  }
-  return genderCodesToReturn
-}
-
-const bothClientsHaveDOB = (
-  includeGenderCodes,
-  primaryClient,
-  secondaryClient,
-  primaryClientIsYounger,
-  secondaryClientIsYounger,
-  genderBothKnown,
-  genderBothUnknown,
-  genderPriUnknownSecKnown,
-  genderPriKnownSecUnknown
-) => {
-  let genderCodesReturned = []
-  if (genderBothKnown) {
-    genderCodesReturned = whenBothHaveDOBandGender(
-      primaryClient,
-      secondaryClient,
-      primaryClientIsYounger,
-      secondaryClientIsYounger
-    )
-    includeGenderCodes.push(genderCodesReturned, `U${secondaryClient.gender_code}`, `${primaryClient.gender_code}U`, 'UU')
-  }
-
-  if (genderPriUnknownSecKnown) {
-    genderCodesReturned = whenBothHaveDOBnoPrmaryGender(secondaryClient, primaryClientIsYounger, secondaryClientIsYounger)
-    includeGenderCodes = includeGenderCodes.concat(genderCodesReturned)
-    includeGenderCodes.push('UU')
-  }
-  if (genderPriKnownSecUnknown) {
-    genderCodesReturned = whenBothHaveDOBnoSecndryGender(primaryClient, primaryClientIsYounger, secondaryClientIsYounger)
-    includeGenderCodes = includeGenderCodes.concat(genderCodesReturned)
-    includeGenderCodes.push('UU')
-  }
-  if (genderBothUnknown) {
-    genderCodesReturned = whenBothHaveDOBnoGender(primaryClientIsYounger, secondaryClientIsYounger)
-    includeGenderCodes = includeGenderCodes.concat(genderCodesReturned)
-  }
-  return includeGenderCodes
-}
-
-const bothClientsDontHaveDOB = (
-  includeGenderCodes,
-  primaryClient,
-  secondaryClient,
-  genderBothKnown,
-  genderBothUnknown,
-  genderPriUnknownSecKnown,
-  genderPriKnownSecUnknown
-) => {
-  let genderCodesReturned = []
-  if (genderBothKnown) {
-    includeGenderCodes.push(primaryClient.gender_code.toLowerCase() + secondaryClient.gender_code)
-    includeGenderCodes.push(primaryClient.gender_code + secondaryClient.gender_code.toLowerCase())
-    includeGenderCodes.push(`U${secondaryClient.gender_code}`, `${primaryClient.gender_code}U`, 'UU')
-  }
-  if (
-    genderBothUnknown ||
-    genderPriUnknownSecKnown ||
-    genderPriKnownSecUnknown
-  ) {
-    genderCodesReturned = noDOBnoGender(
-      primaryClient,
-      secondaryClient,
-      genderBothUnknown,
-      genderPriUnknownSecKnown,
-      genderPriKnownSecUnknown
-    )
-    includeGenderCodes = includeGenderCodes.concat(genderCodesReturned)
-  }
-  return includeGenderCodes
-}
-const checkGivenData = (primaryClient, secondaryClient) => {
-  const bothHaveDOB = doBothHaveDOB(primaryClient, secondaryClient)
-  const genderBothKnown = areGenderBothKnown(primaryClient, secondaryClient)
-  const genderBothUnknown = areGenderBothUnknown(primaryClient, secondaryClient)
-  const genderPriUnknownSecKnown = isGenderPriUnknownSecKnown(primaryClient, secondaryClient)
-  const genderPriKnownSecUnknown = isGenderPriKnownSecUnknown(primaryClient, secondaryClient)
-  const primaryClientBirthDate = getClientBirthDate(primaryClient)
-  const secondaryClientBirthDate = getClientBirthDate(secondaryClient)
-  const primaryClientIsYounger = isPrimaryClientYounger(primaryClientBirthDate, secondaryClientBirthDate)
-  const secondaryClientIsYounger = isSecondaryClientYounger(primaryClientBirthDate, secondaryClientBirthDate)
-  return {
-    bothHaveDOB: bothHaveDOB,
-    genderBothKnown: genderBothKnown,
-    genderBothUnknown: genderBothUnknown,
-    genderPriUnknownSecKnown: genderPriUnknownSecKnown,
-    genderPriKnownSecUnknown: genderPriKnownSecUnknown,
-    primaryClientIsYounger: primaryClientIsYounger,
-    secondaryClientIsYounger: secondaryClientIsYounger,
-  }
-}
-const relationshipDropdown = (primaryClient, secondaryClient) => {
-  const parsedData = checkGivenData(primaryClient, secondaryClient)
-  let includeGenderCodes = []
-  if (!parsedData.genderBothUnknown) {
-    includeGenderCodes.push(primaryClient.gender_code + secondaryClient.gender_code)
-  }
-  if (parsedData.bothHaveDOB) {
-    includeGenderCodes = bothClientsHaveDOB(includeGenderCodes, primaryClient, secondaryClient, parsedData.primaryClientIsYounger,
-      parsedData.secondaryClientIsYounger, parsedData.genderBothKnown, parsedData.genderBothUnknown, parsedData.genderPriUnknownSecKnown,
-      parsedData.genderPriKnownSecUnknown)
-  }
-  if (!parsedData.bothHaveDOB) {
-    includeGenderCodes = bothClientsDontHaveDOB(includeGenderCodes, primaryClient, secondaryClient, parsedData.genderBothKnown,
-      parsedData.genderBothUnknown, parsedData.genderPriUnknownSecKnown, parsedData.genderPriKnownSecUnknown)
-  }
-  includeGenderCodes = includeGenderCodes.map((rec) => rec.replace(/I/g, 'U'))
-  return RELATIONSHIP_TYPES.filter((rec) =>
-    includeGenderCodes.includes(rec.gender_code)
-  ).filter((rec) => !rec.label.match(/\*/))
-}
 export default relationshipDropdown
