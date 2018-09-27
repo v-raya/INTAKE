2b2f0ef75ba5a478fcb3345683c13c2724c936aa
Tyler Clemens <Tyler.Clemens@osi.ca.gov>
2018-09-27 09:36:19 -0700

    use new make_api_call params in RelationshipsRepository
    SNAP-671

--- app/repositories/relationships_repository.rb
+++ app/repositories/relationships_repository.rb
@@ -3,51 +3,58 @@
 # RelationshipsRepository is a service class responsible for retrieval of
 # relationships via the API
 class RelationshipsRepository
-  def self.create(security_token, relationships)
+  def self.create(security_token, request_id, relationships)
     return [] if relationships.blank?
     FerbAPI.make_api_call(
-      security_token,
-      FerbRoutes.screening_relationships,
-      :post,
-      relationships
+      security_token: security_token,
+      request_id: request_id,
+      url: FerbRoutes.screening_relationships,
+      method: :post,
+      payload: relationships
     ).body
   end
 
-  def self.search(security_token, client_ids)
+  def self.search(security_token, request_id, client_ids)
     return [] if client_ids.blank?
     FerbAPI.make_api_call(
-      security_token,
-      FerbRoutes.relationships_path,
-      :get,
-      clientIds: client_ids
+      security_token: security_token,
+      request_id: request_id,
+      url: FerbRoutes.relationships_path,
+      method: :get,
+      payload: {
+        clientIds: client_ids
+      }
     ).body
   end
 
-  def self.get_relationships_for_screening_id(security_token, screeing_id)
-    return [] if screeing_id.blank?
+  def self.get_relationships_for_screening_id(security_token, request_id, screening_id)
+    return [] if screening_id.blank?
     FerbAPI.make_api_call(
-      security_token,
-      FerbRoutes.relationships_for_screening_path(screeing_id),
-      :get
+      security_token: security_token,
+      request_id: request_id,
+      url: FerbRoutes.relationships_for_screening_path(screening_id),
+      method: :get
     ).body
   end
 
-  def self.find(security_token, id)
+  def self.find(security_token, request_id, id)
     raise StandardError, 'Error updating relationship: id is required' if id.blank?
     FerbAPI.make_api_call(
-      security_token,
-      FerbRoutes.screening_relationship_path(id),
-      :get
+      security_token: security_token,
+      request_id: request_id,
+      url: FerbRoutes.screening_relationship_path(id),
+      method: :get
     ).body
   end
 
-  def self.update(security_token, id, relationship)
+  def self.update(security_token, request_id, id, relationship)
     raise StandardError, 'Error updating relationship: id is required' if id.blank?
     FerbAPI.make_api_call(
-      security_token,
-      FerbRoutes.screening_relationship_path(id),
-      :put,
-      relationship
+      security_token: security_token,
+      request_id: request_id,
+      url: FerbRoutes.screening_relationship_path(id),
+      method: :put,
+      payload: relationship
     ).body
   end
 end
