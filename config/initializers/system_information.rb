ae851ee7a89bf7282a303c7181d9c80436ed339e
Adarsh Vandana <adarsh.vandana@osi.ca.gov>
2018-11-02 16:48:05 -0700

    Jenkins build fix

--- config/initializers/system_information.rb
+++ config/initializers/system_information.rb
@@ -2,10 +2,13 @@
 
 SystemInformation.configure do |config|
     config.application = 'Intake App'
-    config.version = "#{ENV.fetch('APP_VERSION', 'unknown')}"
+    config.version = ENV.fetch('APP_VERSION', 'unknown').to_s
     config.checks =
       [
-        { name: :redis, url: "redis://#{ENV.fetch('REDIS_HOST', 'localhost')}:#{ENV.fetch('REDIS_PORT', 6379)}" },
-        { name: :perry, url: "#{ENV.fetch('AUTHENTICATION_API_BASE_URL', 'http://localhost/perry')}/system-information" }
+        { name: :redis, url: "redis://#{ENV.fetch('REDIS_HOST', 'localhost')}:
+                  #{ENV.fetch('REDIS_PORT', 6379)}" },
+        { name: :perry, url: "#{ENV.fetch('AUTHENTICATION_API_BASE_URL', 
+                  'http://localhost/perry')}/system-information" }
       ]
   end
+  
