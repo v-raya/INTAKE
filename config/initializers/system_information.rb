df512b25ea94f6e1f47ff18dc2e936ae22ab56d5
Adarsh Vandana <adarsh.vandana@osi.ca.gov>
2018-11-02 17:03:52 -0700

    Jenkins build fix

--- config/initializers/system_information.rb
+++ config/initializers/system_information.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
 SystemInformation.configure do |config|
-    config.application = 'Intake App'
-    config.version = ENV.fetch('APP_VERSION', 'unknown').to_s
-    config.checks =
-      [
-        { name: :redis, url: "redis://#{ENV.fetch('REDIS_HOST', 'localhost')}:
-                  #{ENV.fetch('REDIS_PORT', 6379)}" },
-        { name: :perry, url: "#{ENV.fetch('AUTHENTICATION_API_BASE_URL', 
-                  'http://localhost/perry')}/system-information" }
-      ]
-  end
-  
+  config.application = 'Intake App'
+  config.version = ENV.fetch('APP_VERSION', 'unknown').to_s
+  config.checks =
+    [
+      { name: :redis,
+        url: "redis://#{ENV.fetch('REDIS_HOST', 'localhost')}:"\
+          "#{ENV.fetch('REDIS_PORT', 6379)}" },
+      { name: :perry,
+        url: "#{ENV.fetch('AUTHENTICATION_API_BASE_URL', 'http://localhost/perry')}/"\
+          'system-information' }
+    ]
+end
