a26b134c95dfab0ca2e912bc14a68f8aa6a8c664
Adarsh Vandana <adarsh.vandana@osi.ca.gov>
2018-11-28 09:22:30 -0800

    FIT-284 cleanup removed the used Script

--- scripts/ci/compute_version.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-# frozen_string_literal: true
-
-base_version = `git describe --tags $(git rev-list --tags --max-count=1)`.chomp
-base_version << ".#{ENV['BUILD_NUMBER']}" if ENV['BUILD_NUMBER']
-puts base_version
